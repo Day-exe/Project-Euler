093e1bf239c4b9a07fb7b70bec5f943e7e0dc2f9
Day <ethanscoggin1999@gmail.com>
2025-01-23 12:11:18 -0500

    n/s

--- src/EulerPrograms/Project100/arrangedProbability.java
+++ src/EulerPrograms/Project100/arrangedProbability.java
@@ -5,9 +5,11 @@ import EulerPrograms.Library;
 public class arrangedProbability {
 
     public static void main(String[] args) {
+
         long totalDiscs = (long) Math.pow(10, 12);
         long blueDiscs = Library.findBlueDiscs(totalDiscs);
         System.out.println("The number of blue discs in the first arrangement exceeding " + totalDiscs + " total discs is: " + blueDiscs);
+    
     }
 
 }
