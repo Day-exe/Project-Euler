3c975ab326eaa17cb385efdbaf3d92f4e84778e5
Day <ethanscoggin1999@gmail.com>
2025-01-23 10:06:31 -0500

    n/s

--- src/EulerPrograms/Project2/evenFibonacciNumbers.java
+++ src/EulerPrograms/Project2/evenFibonacciNumbers.java
@@ -5,9 +5,11 @@ import EulerPrograms.Library;
 public class evenFibonacciNumbers {
     
     public static void main(String[] args) {
+
         int limit = 4000000;
         int sum = Library.calculateEvenFibonacciSum(limit);
         System.out.println("The sum of even Fibonacci numbers not exceeding " + limit + " is: " + sum);
+        
     }
 
 }
